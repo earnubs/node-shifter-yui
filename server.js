0bee2cab19c291c48bbf4403ca3df3a05a7a0441
Stephen Stewart <stephen.stewart@carisenda.com>
2012-09-17 13:01:22 -0400

    add yui3 as a submodule

--- server.js
+++ server.js
@@ -3,15 +3,19 @@ var express = require('express'),
 
     app = express();
 
-app.set('title', 'Hello World');
+// test page making reqs for yui js
+app.set('title', 'YUI Build Trial');
 app.set('views', __dirname + '/views');
 
 app.get('/', function(req, res){
       res.render('index.jade', { title: app.get('title') });
 });
 
-app.get('/u1', combo.combine({rootPath: 'u1-js-modules'}), function (req, res) {
-    res.send(res.body, 200);
+app.get('/yui3', combo.combine({rootPath: 'yui3'}), function (req, res) {
+        res.send(res.body, 200);
+});
+app.get('/yui3-u1', combo.combine({rootPath: 'yui3'}), function (req, res) {
+        res.send(res.body, 200);
 });
 
 app.listen(3000);
