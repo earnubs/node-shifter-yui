58dc1f1b80e607d6acf1d27292c28e1c4792dfbc
Stephen Stewart <stephen.stewart@carisenda.com>
2012-09-20 09:20:02 -0400

    show pattern and module options in loader config

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express'),
 
     app = express();
 
-// test page making reqs for yui js
+// just a simple test page 
 app.set('title', 'YUI Build Trial');
 app.set('views', __dirname + '/views');
 
@@ -11,12 +11,14 @@ app.get('/', function(req, res){
       res.render('index.jade', { title: app.get('title') });
 });
 
+// requests to the local yui3 lib
 app.get('/yui3', combo.combine({rootPath: 'yui3'}), function (req, res) {
-        res.send(res.body, 200);
+    res.send(res.body, 200);
 });
+
+// requests to your own yui3 lib
 app.get('/yui3-contrib', combo.combine({rootPath: 'yui3-contrib'}), function (req, res) {
-    console.log('hiya');
-        res.send(res.body, 200);
+    res.send(res.body, 200);
 });
 
 app.listen(3000);
